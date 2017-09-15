b3cb3daab04d350a92d149c962ed88e3e0d8328f
Jose Cáceres <joseluis8906@opmbx.org>
2017-09-15 08:06:13 -0500

    actualizacion

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -27,7 +27,7 @@ v-app(id="sandbox" :dark="dark" :light="!dark" standalone)
       nuxt
 
   v-footer(fixed)
-    <span> © {{ new Date().getFullYear() }} </span> Jose Luis Cáceres Escudero
+    <span> © {{ new Date().getFullYear() }} </span> Julio Cesar Caicedo Santos & Jose Luis Cáceres Escudero
 
 </template>
 
